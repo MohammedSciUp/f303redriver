c10448ca7937cfeb609f844133dc5f992dcba078
alzarii <mohammed101alzarii>
2024-06-11 19:35:12 +0400

    wrong folder correction

---
+++ driverlib/Debug/Src/main.d
@@ -0,0 +1,3 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/moham/Desktop/STM32\ development/embeddedC/workspace/target/driverlib/driverfiles/Inc/devicef303re.h
+C:/Users/moham/Desktop/STM32\ development/embeddedC/workspace/target/driverlib/driverfiles/Inc/devicef303re.h:
